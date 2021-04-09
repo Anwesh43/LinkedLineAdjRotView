b35a8571e94bd5278fa595a9925cd2fb8683a2a5
Anwesh43 <anweshthecool0@gmail.com>
2021-04-10 03:42:21 +0530

    adding Canvas extension methods

--- LineAdjRotView/src/main/java/com/example/lineadjrotview/LineAdjRotView.kt
+++ LineAdjRotView/src/main/java/com/example/lineadjrotview/LineAdjRotView.kt
@@ -29,3 +29,39 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawLineAdjRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val lSize : Float = Math.min(w, h) / lSizeFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val sf4 : Float = sf.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        rotate(90f * j * sf2)
+        drawLine(0f, 0f, size * sf1, 0f, paint)
+        restore()
+    }
+
+    for (j in 0..1) {
+        save()
+        translate(lSize, -lSize)
+        rotate(90f * sf4)
+        drawLine(0f, 0f, 0f, lSize * sf3, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawLARNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineAdjRot(scale, w, h, paint)
+}
